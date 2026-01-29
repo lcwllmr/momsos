502cf4e2139c00abe1c9a5b1dcf6d40c98b7902c
Luca Wellmeier <159539641+lcwllmr@users.noreply.github.com>
2026-01-29 18:34:03 +0530

    code: add experiment for weighted sum of squares

--- code/momsos/experiments/motzkin_not_sos.py
+++ code/momsos/experiments/motzkin_not_sos.py
@@ -1,20 +1,37 @@
+import math
 import cvxpy as cp
 from momsos.poly import Polynomial, PolynomialRing
 
-def main():
-    motzkin = Polynomial.motzkin()
-    v_nd = PolynomialRing(motzkin.n).monomials_upto(motzkin.degree // 2)
+def attempt_sos_decomposition(target):
+    d1 = math.ceil(target.degree / 2)
+    d2 = 2 * d1
+    v_nd = PolynomialRing(target.n).monomials_upto(d1)
     Q = cp.Variable((len(v_nd), len(v_nd)), PSD=True)
-    pQ = Polynomial.from_gram_matrix(motzkin.n, motzkin.degree // 2, Q) # p = 
+    pQ = Polynomial.from_gram_matrix(target.n, d1, Q)
 
     linear_equality_constraints = []
-    for m in PolynomialRing(motzkin.n).monomials_upto(motzkin.degree):
-        linear_equality_constraints.append(motzkin[m] == pQ[m])
+    for m in PolynomialRing(target.n).monomials_upto(d2):
+        linear_equality_constraints.append(target[m] == pQ[m])
 
-    # we don't need an optimization target because we are just interested in whether
-    # there is a solution -> a feasibility problem
+    # NOTE: we don't need an optimization target because just need
+    # to know whether there is a solution (feasibility problem)
     sdp = cp.Problem(cp.Minimize(0), linear_equality_constraints)
-    sdp.solve(solver=cp.CLARABEL, verbose=True)
+    sdp.solve(solver=cp.CLARABEL)
+
+    print("  ...  Computation finished.")
+    print("  Solution status:", sdp.status)
+    print(f"  Solve time: {sdp.solver_stats.solve_time}s")
+    print(f"  Iterations: {sdp.solver_stats.num_iters}")
+    
+
+def main():
+    print('\nStarting test: Is M(x,y) a sum of squares?', end='')
+    motzkin = Polynomial.motzkin()
+    attempt_sos_decomposition(motzkin)
+
+    print('\nStarting test: Is (x^2 + y^2) * M(x,y) a sum of squares?', end='')
+    weighted_motzkin = Polynomial({(2,0): 1.0, (0,2): 1.0}) * Polynomial.motzkin()
+    attempt_sos_decomposition(weighted_motzkin)
 
 if __name__ == "__main__":
     main()
